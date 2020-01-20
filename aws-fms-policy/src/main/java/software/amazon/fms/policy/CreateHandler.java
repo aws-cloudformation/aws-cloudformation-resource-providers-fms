63b86ee9b5e71a47ab5f37338b0f4b7c3ac39e6a
Sheldon Woodward <sheldowo@amazon.com>
2020-01-20 11:51:54 -0800

    Added generic exception handling to post-PutPolicy code to delete policies unknown to CloudFormation.

--- aws-fms-policy/src/main/java/software/amazon/fms/policy/CreateHandler.java
+++ aws-fms-policy/src/main/java/software/amazon/fms/policy/CreateHandler.java
@@ -1,7 +1,9 @@
 package software.amazon.fms.policy;
 
+import software.amazon.awssdk.services.fms.model.DeletePolicyRequest;
 import software.amazon.awssdk.services.fms.model.PutPolicyRequest;
 import software.amazon.awssdk.services.fms.model.PutPolicyResponse;
+import software.amazon.cloudformation.exceptions.CfnInternalFailureException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
@@ -30,10 +32,21 @@ public class CreateHandler extends PolicyHandler<PutPolicyResponse> {
             final ResourceHandlerRequest<ResourceModel> request,
             final AmazonWebServicesClientProxy proxy) {
 
-        // convert the create request response to a resource model and add the tags in
-        return CfnHelper.convertFMSPolicyToCFNResourceModel(
-                response.policy(),
-                response.policyArn(),
-                FmsHelper.convertCFNTagMapToFMSTagSet(request.getDesiredResourceTags()));
+        try {
+            // convert the create request response to a resource model and add the tags in
+            return CfnHelper.convertFMSPolicyToCFNResourceModel(
+                    response.policy(),
+                    response.policyArn(),
+                    FmsHelper.convertCFNTagMapToFMSTagSet(request.getDesiredResourceTags()));
+        } catch (Exception e) {
+            // if any code fails, delete the policy since CloudFormation is unaware of it
+            DeletePolicyRequest deletePolicyRequest = DeletePolicyRequest.builder()
+                    .policyId(response.policy().policyId())
+                    .build();
+            proxy.injectCredentialsAndInvokeV2(deletePolicyRequest, client::deletePolicy);
+
+            // raise an internal exception so CloudFormation knows policy creation failed
+            throw new CfnInternalFailureException(e);
+        }
     }
 }
