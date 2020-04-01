0b27ae90379daa0eda5d2bc30b0ffc9ef08df1e0
Sheldon Woodward <sheldowo@amazon.com>
2020-04-01 16:53:36 -0700

    Temprorary fix to give customer a better error when attempting to update stacks. (#12)

--- aws-fms-notificationchannel/src/test/java/software/amazon/fms/notificationchannel/UpdateHandlerTest.java
+++ aws-fms-notificationchannel/src/test/java/software/amazon/fms/notificationchannel/UpdateHandlerTest.java
@@ -11,11 +11,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.services.fms.model.FmsRequest;
 import software.amazon.awssdk.services.fms.model.GetNotificationChannelRequest;
 import software.amazon.awssdk.services.fms.model.GetNotificationChannelResponse;
-import software.amazon.awssdk.services.fms.model.InternalErrorException;
-import software.amazon.awssdk.services.fms.model.InvalidOperationException;
-import software.amazon.awssdk.services.fms.model.PutNotificationChannelRequest;
-import software.amazon.awssdk.services.fms.model.PutNotificationChannelResponse;
-import software.amazon.awssdk.services.fms.model.ResourceNotFoundException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
@@ -23,12 +18,9 @@ import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -66,56 +58,6 @@ class UpdateHandlerTest {
     }
 
     @Test
-    void handleRequestSuccess() {
-        // stub the response for the read request
-        final GetNotificationChannelResponse describeGetResponse = GetNotificationChannelResponse.builder()
-                .snsTopicArn(sampleSnsTopicArn)
-                .snsRoleName(sampleSnsRoleName)
-                .build();
-        doReturn(describeGetResponse)
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(GetNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // stub the response for the update request
-        final PutNotificationChannelResponse describePutResponse = PutNotificationChannelResponse.builder().build();
-        doReturn(describePutResponse)
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(PutNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // create the update request and send it
-        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
-                .desiredResourceState(model)
-                .build();
-        final ProgressEvent<ResourceModel, CallbackContext> response =
-                handler.handleRequest(proxy, request, null, logger);
-
-        // verify stub calls
-        verify(proxy, times(2)).injectCredentialsAndInvokeV2(captor.capture(), any());
-        assertThat(captor.getAllValues()).isEqualTo(Arrays.asList(
-                GetNotificationChannelRequest.builder().build(),
-                PutNotificationChannelRequest.builder()
-                        .snsTopicArn(sampleSnsTopicArn)
-                        .snsRoleName(sampleSnsRoleName)
-                        .build()
-        ));
-
-        // assertions
-        assertThat(response).isNotNull();
-        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
-        assertThat(response.getCallbackContext()).isNull();
-        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
-        assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
-        assertThat(response.getResourceModels()).isNull();
-        assertThat(response.getErrorCode()).isNull();
-    }
-
-    @Test
     void handleRequestReadResourceNotFound() {
         // stub the response for the read request
         final GetNotificationChannelResponse describeGetResponse = GetNotificationChannelResponse.builder().build();
@@ -148,24 +90,16 @@ class UpdateHandlerTest {
     }
 
     @Test
-    void handleRequestUpdateNotFoundException() {
+    void handleExpectedUpdateFailure() {
         // stub the response for the read request
         final GetNotificationChannelResponse describeGetResponse = GetNotificationChannelResponse.builder()
-                .snsTopicArn(sampleSnsTopicArn)
                 .snsRoleName(sampleSnsRoleName)
+                .snsTopicArn(sampleSnsTopicArn)
                 .build();
         doReturn(describeGetResponse)
                 .when(proxy)
                 .injectCredentialsAndInvokeV2(
                         ArgumentMatchers.isA(GetNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // mock a ResourceNotFoundException from the FMS API
-        doThrow(ResourceNotFoundException.builder().build())
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(PutNotificationChannelRequest.class),
                         ArgumentMatchers.any()
                 );
 
@@ -177,14 +111,8 @@ class UpdateHandlerTest {
                 handler.handleRequest(proxy, request, null, logger);
 
         // verify stub calls
-        verify(proxy, times(2)).injectCredentialsAndInvokeV2(captor.capture(), any());
-        assertThat(captor.getAllValues()).isEqualTo(Arrays.asList(
-                GetNotificationChannelRequest.builder().build(),
-                PutNotificationChannelRequest.builder()
-                        .snsTopicArn(sampleSnsTopicArn)
-                        .snsRoleName(sampleSnsRoleName)
-                        .build()
-        ));
+        verify(proxy, times(1)).injectCredentialsAndInvokeV2(captor.capture(), any());
+        assertThat(captor.getValue()).isEqualTo(GetNotificationChannelRequest.builder().build());
 
         // assertions
         assertThat(response).isNotNull();
@@ -193,104 +121,6 @@ class UpdateHandlerTest {
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModel()).isNull();
         assertThat(response.getResourceModels()).isNull();
-        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.NotFound);
-    }
-
-    @Test
-    void handleRequestUpdateInvalidOperationException() {
-        // stub the response for the read request
-        final GetNotificationChannelResponse describeGetResponse = GetNotificationChannelResponse.builder()
-                .snsTopicArn(sampleSnsTopicArn)
-                .snsRoleName(sampleSnsRoleName)
-                .build();
-        doReturn(describeGetResponse)
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(GetNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // mock a InvalidOperationException from the FMS API
-        doThrow(InvalidOperationException.builder().build())
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(PutNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // create the update request and send it
-        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
-                .desiredResourceState(model)
-                .build();
-        final ProgressEvent<ResourceModel, CallbackContext> response =
-                handler.handleRequest(proxy, request, null, logger);
-
-        // verify stub calls
-        verify(proxy, times(2)).injectCredentialsAndInvokeV2(captor.capture(), any());
-        assertThat(captor.getAllValues()).isEqualTo(Arrays.asList(
-                GetNotificationChannelRequest.builder().build(),
-                PutNotificationChannelRequest.builder()
-                        .snsTopicArn(sampleSnsTopicArn)
-                        .snsRoleName(sampleSnsRoleName)
-                        .build()
-        ));
-
-        // assertions
-        assertThat(response).isNotNull();
-        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
-        assertThat(response.getCallbackContext()).isNull();
-        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
-        assertThat(response.getResourceModel()).isNull();
-        assertThat(response.getResourceModels()).isNull();
-        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
-    }
-
-    @Test
-    void handleRequestUpdateInternalErrorException() {
-        // stub the response for the read request
-        final GetNotificationChannelResponse describeGetResponse = GetNotificationChannelResponse.builder()
-                .snsTopicArn(sampleSnsTopicArn)
-                .snsRoleName(sampleSnsRoleName)
-                .build();
-        doReturn(describeGetResponse)
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(GetNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // mock a InvalidOperationException from the FMS API
-        doThrow(InternalErrorException.builder().build())
-                .when(proxy)
-                .injectCredentialsAndInvokeV2(
-                        ArgumentMatchers.isA(PutNotificationChannelRequest.class),
-                        ArgumentMatchers.any()
-                );
-
-        // create the update request and send it
-        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
-                .desiredResourceState(model)
-                .build();
-        final ProgressEvent<ResourceModel, CallbackContext> response =
-                handler.handleRequest(proxy, request, null, logger);
-
-        // verify stub calls
-        verify(proxy, times(2)).injectCredentialsAndInvokeV2(captor.capture(), any());
-        assertThat(captor.getAllValues()).isEqualTo(Arrays.asList(
-                GetNotificationChannelRequest.builder().build(),
-                PutNotificationChannelRequest.builder()
-                        .snsTopicArn(sampleSnsTopicArn)
-                        .snsRoleName(sampleSnsRoleName)
-                        .build()
-        ));
-
-        // assertions
-        assertThat(response).isNotNull();
-        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
-        assertThat(response.getCallbackContext()).isNull();
-        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
-        assertThat(response.getResourceModel()).isNull();
-        assertThat(response.getResourceModels()).isNull();
-        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.ServiceInternalError);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.AlreadyExists);
     }
 }
